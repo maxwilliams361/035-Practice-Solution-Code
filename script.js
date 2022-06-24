e029bc0309eab025f0eaea2a76d942c6d24f4437
maxwilliams361 <52715538+maxwilliams361@users.noreply.github.com>
2022-06-24 14:13:24 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,7 @@
+let title=document.querySelector(".title");
+let storyOpening=document.querySelector(".storyOpening");
+let openBoxDiv=document.querySelector(".partOne-option1");
+let runAwayDiv=document.querySelector(".partOne-option2");
+
+openBoxDiv.style="display:none";
+runAwayDiv.style="display:none";
